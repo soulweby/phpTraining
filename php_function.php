a616e39939dc5a21d4b1a477f73e7655ed599f0d
soulweby <souleyso47@gmail.com>
2022-10-14 17:58:29 +0200

    array exercices end

---
+++ php_function.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php training</title>
+</head>
+
+<body style="background-color:#303; color: white">
+    <p>///********************\\\</p>
+    <h1>Training PHP</h1>
+    <p>///********************\\\</p>
+
+    <?php
+
+    date_default_timezone_set('UTC');
+
+    $showDate = date('l jS \of F Y h:i:s A');
+
+    echo $showDate;
+    echo "<br />";
+    echo "<br />";
+
+
+
+    ?>
+
+
+</body>
+
+</html>
